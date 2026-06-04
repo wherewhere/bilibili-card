8dff18714c987b5ee1b512bb41cff496ff9aeaa4
where where <qq2518766683@outlook.com>
2026-06-04 14:18:09 +0800

    添加是否已注册判断

--- src/components/bilibili-card/register.ts
+++ src/components/bilibili-card/register.ts
@@ -1,4 +1,6 @@
 import BiliBiliCard from ".";
 import { dom } from "../../helpers/dom";
-dom.customElements.define("bilibili-card", BiliBiliCard);
+if (!dom.customElements.get("bilibili-card")) {
+    dom.customElements.define("bilibili-card", BiliBiliCard);
+}
 export default BiliBiliCard;
